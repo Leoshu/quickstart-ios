c5183f9a42dcb472ac2fd8e73182146695ebadd0
Leo_hsu <leo_hsu@gemteks.com>
2017-01-19 18:26:11 +0800

    Add ServiceExtension.

---
+++ messaging/ServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  ServiceExtension
+//
+//  Created by Leo_hsu on 2017/1/19.
+//  Copyright © 2017年 Google Inc. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
